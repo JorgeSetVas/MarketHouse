a9fbfdadf3fb403ddfdae079da4dc422e7a8aeb1
Jorge Vásquez <jas.vasquezc@gmail.com>
2021-02-22 10:39:57 -0600

    commit

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Route, Swithc } from 'react-router-dom';
+import 
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
